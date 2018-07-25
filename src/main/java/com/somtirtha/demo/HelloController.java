d15bd825a19a51d5eb685679bae8d7f5554d3fc8
Somtirtha Roy <somtirtha.roy@gmail.com>
2018-07-25 10:50:15 -0600

    Initial Commit

---
+++ src/main/java/com/somtirtha/demo/HelloController.java
@@ -0,0 +1,15 @@
+package com.somtirtha.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello from Spring!";
+    }
+
+}
+
